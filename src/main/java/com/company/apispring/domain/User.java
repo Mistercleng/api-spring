0b820d8da9cbb99a8ea83a6c359789e293248cd3
mistercleng <mrcleng@gmail.com>
2022-07-10 16:50:18 -0400

    Create class User

---
+++ src/main/java/com/company/apispring/domain/User.java
@@ -0,0 +1,23 @@
+package com.company.apispring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}

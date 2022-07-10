4943078e5a3cfee769d18a43308e76fb39d806d7
mistercleng <mrcleng@gmail.com>
2022-07-10 17:18:08 -0400

    interface user repository

---
+++ src/main/java/com/company/apispring/domain/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.company.apispring.domain.repositories;
+
+import com.company.apispring.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Integer>{
+}

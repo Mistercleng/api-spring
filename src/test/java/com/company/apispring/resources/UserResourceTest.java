7d8a40af866be5d8feed3a7f60f39e30dd26497e
mistercleng <mrcleng@gmail.com>
2022-07-11 21:24:53 -0400

    test endpoint class UserResources findById

--- src/test/java/com/company/apispring/resources/UserResourceTest.java
+++ src/test/java/com/company/apispring/resources/UserResourceTest.java
@@ -3,17 +3,23 @@ package com.company.apispring.resources;
 import com.company.apispring.domain.UserCompany;
 import com.company.apispring.domain.dto.UserDTO;
 import com.company.apispring.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -44,7 +50,23 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdSuccess() {
+        when(serviceMock.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resourceMock.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class,response.getClass());
+        assertEquals(UserDTO.class,response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getNome());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
+
+
     }
 
     @Test
